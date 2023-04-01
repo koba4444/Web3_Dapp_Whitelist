492d6cd746314f92a6c5c41dfc70dd95009690cb
kok4444 <koba4444@gmail.com>
2023-04-01 16:06:34 +0300

    changed testnet in my-app

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -42,9 +42,9 @@ export default function Home() {
 
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
+    if (chainId !== 11155111) {
+      window.alert("Change the network to Sepolia");
+      throw new Error("Change network to Sepolia");
     }
 
     if (needSigner) {
@@ -187,7 +187,7 @@ export default function Home() {
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
-        network: "goerli",
+        network: "sepolia",
         providerOptions: {},
         disableInjectedProvider: false,
       });
@@ -207,7 +207,7 @@ export default function Home() {
           <h1 className={styles.title}>Welcome to KOK Arts!</h1>
           <div className={styles.description}>
             {/* Using HTML Entities for the apostrophe */}
-            It&#39;s an NFT collection for developers in Crypto.
+            It&#39;s an NFT collection for developers in KOK Arts.
           </div>
           <div className={styles.description}>
             {numberOfWhitelisted} have already joined the Whitelist
